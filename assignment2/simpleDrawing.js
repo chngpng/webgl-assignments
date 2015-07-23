37e09d5b1784a53d1ffbcc558a1db3a08ab4fa13
chngpng <pengcheng85@gmail.com>
2015-07-22 23:28:20 -0400

    use bufferSubData and LINE_STRIP

--- assignment2/simpleDrawing.js
+++ assignment2/simpleDrawing.js
@@ -2,11 +2,11 @@
 
 var gl;
 
-var delay = 100;
-var direction = true;
+var maxNumPoints = 5000;
 var beginDrawing = false;
 
-var vertices = [];
+var index = 0;
+var lineStartEnd = [];
 
 window.onload = function init()
 {
@@ -24,42 +24,39 @@ window.onload = function init()
     gl.clearColor( 0.8, 0.8, 0.8, 1.0 );
     gl.clear( gl.COLOR_BUFFER_BIT );
 
-    console.log("canvas width: " + canvas.width + ", height: " + canvas.height);
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, maxNumPoints, gl.STATIC_DRAW);
 
     canvas.addEventListener("mousedown", function(event) {
-      console.log("mousedown event!");
       beginDrawing = true;
       var rect = event.target.getBoundingClientRect();
-      console.log("rect left: " + rect.left + ", right: " + rect.right);
       var x = (event.clientX - rect.left - canvas.width / 2) / (canvas.width / 2);
       var y = (canvas.height / 2  + rect.top - event.clientY) / (canvas.height / 2);
 
-      if (vertices.length > 0) vertices.pop();
-      vertices.push(vec2(x, y));
+      gl.bufferSubData(gl.ARRAY_BUFFER, 8 * index , flatten(vec2(x, y)));
+      index++;
+      lineStartEnd.push([index, index]);
     });
 
     // Note that this event listener is registered on the window because everything time the
     // mouseup event should stop drawing no matter where the mouse is in or out of the canvas.
     window.addEventListener("mouseup", function(event) {
-      console.log("mouseup within window");
       beginDrawing = false;
     })
 
     // Note that this event handler is registered on the canvas because only start drawing if the
     // mousedown event is happening when the mouse is in the canvas boundary.
     canvas.addEventListener("mousemove", function(event) {
-      if (beginDrawing) console.log("mousemove event: x: " + event.clientX + ", y: " + event.clientY);
       var rect = event.target.getBoundingClientRect();
       if (beginDrawing) {
         var x = (event.clientX - rect.left - canvas.width / 2) / (canvas.width / 2);
         var y = (canvas.height / 2  + rect.top - event.clientY) / (canvas.height / 2);
 
-        vertices.push(vec2(x, y));
-        vertices.push(vec2(x, y));
+        gl.bufferSubData(gl.ARRAY_BUFFER, 8 * index , flatten(vec2(x, y)));
+        index++;
+        lineStartEnd[lineStartEnd.length - 1][1] = index;
 
-        var vBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
         var vPosition = gl.getAttribLocation( program, "vPosition");
         gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(vPosition);
@@ -68,7 +65,6 @@ window.onload = function init()
     });
 
     //  Load shaders and initialize attribute buffers
-
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 };
@@ -77,5 +73,6 @@ function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT );
     gl.lineWidth(5);
-    gl.drawArrays(gl.LINES, 0, vertices.length);
+    for (var i = 0; i < lineStartEnd.length; i++)
+      gl.drawArrays(gl.LINE_STRIP, lineStartEnd[i][0] - 1, lineStartEnd[i][1] - lineStartEnd[i][0]);
 }
