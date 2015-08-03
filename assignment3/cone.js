34b6633e0e300f183d9c3961e9911aeb847e5162
chngpng <pengcheng85@gmail.com>
2015-08-02 23:03:14 -0400

    drawing multiple cones

--- assignment3/cone.js
+++ assignment3/cone.js
@@ -4,7 +4,10 @@ var canvas;
 var gl;
 
 var draw = false;
-var numTimesToSubdivide = 4;
+var numTimesToSubdivide = 1;
+
+// Each cone gets its own program, with its own shaders.
+var programs = [];
  
 var index = 0;
 
@@ -78,8 +81,9 @@ window.onload = function init() {
     //
     //  Load shaders and initialize attribute buffers
     //
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    //var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    //programs.push(program);
+    //gl.useProgram( program );
     
     var vxpos = vec4(1, 0.0, 0.0, 1);
     var vxneg = vec4(-1, 0.0, 0.0, 1);
@@ -97,19 +101,56 @@ window.onload = function init() {
     divideTriangle(vxneg, vzpos, vcenter, numTimesToSubdivide);
     divideTriangle(vxneg, vzneg, vcenter, numTimesToSubdivide);
 
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+    //var vBuffer = gl.createBuffer();
+    //gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
+    //gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
     
-    var vPosition = gl.getAttribLocation( program, "vPosition");
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray( vPosition);
+    //var vPosition = gl.getAttribLocation( program, "vPosition");
+    //gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0);
+    //gl.enableVertexAttribArray( vPosition);
+    //program.vPosition = gl.getAttribLocation( program, "vPosition");
+    //gl.vertexAttribPointer( program.vPosition, 4, gl.FLOAT, false, 0, 0);
+    //gl.enableVertexAttribArray( program.vPosition);
 
-    fColor = gl.getUniformLocation(program, "fColor");
+    //fColor = gl.getUniformLocation(program, "fColor");
+    //program.fColor = gl.getUniformLocation(program, "fColor");
     
-    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
-    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+    //modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    //projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+    //program.modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    //program.projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
 
+    document.getElementById("draw").onclick = function(event) {
+      console.log("draw button clicked");
+
+      var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+      programs.push(program);
+      //gl.useProgram( program );
+
+      var vBuffer = gl.createBuffer();
+      gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
+      gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+
+      program.vPosition = gl.getAttribLocation( program, "vPosition");
+      gl.vertexAttribPointer( program.vPosition, 4, gl.FLOAT, false, 0, 0);
+      gl.enableVertexAttribArray( program.vPosition);
+
+      program.fColor = gl.getUniformLocation(program, "fColor");
+      program.modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+      program.projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+
+      program.xAngle = xAngle;
+      program.yAngle = yAngle;
+      program.zAngle = zAngle;
+      program.xPos = xPos;
+      program.yPos = yPos;
+      program.zPos = zPos;
+      program.scaleFactor = scaleFactor;
+
+      //init();
+      draw = true;
+      render();
+    }
     document.getElementById("x-angle-slider").onchange = function(event) {
       console.log("x-angle = " + event.target.value);
       xAngle = event.target.value;
@@ -121,7 +162,7 @@ window.onload = function init() {
       //render();
     };
     document.getElementById("z-angle-slider").onchange = function(event) {
-      console.log("z-pos = " + event.target.value);
+      console.log("z-angle = " + event.target.value);
       zAngle = event.target.value;
       //render();
     };
@@ -155,12 +196,7 @@ window.onload = function init() {
       scaleFactor = 2;
       //init();
     };
-    document.getElementById("draw").onclick = function(event) {
-      console.log("draw button clicked");
-      //init();
-      draw = true;
-      render();
-    }
+
     render();
 }
 
@@ -173,44 +209,86 @@ function render() {
     return;
   }
 
-    //eye = vec3(radius*Math.sin(theta)*Math.cos(phi), 
-    //    radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
+  for (var i = 0; i < programs.length; i++) {
+    console.log("process i = " + i);
+    processProgram(programs[i]);
+  }
+
+    //eye = vec3(0, 0, radius);
+
+    //var rotateX = rotate(xAngle, 1, 0, 0) // rotate around the x axis.
+    //var rotateY = rotate(yAngle, 0, 1, 0) // rotate around the x axis.
+    //var rotateZ = rotate(zAngle, 0, 0, 1) // rotate around the x axis.
+
+    //// angle adjustment.
+    //modelViewMatrix = lookAt(eye, at , up);
+    //modelViewMatrix = mult(rotateX, modelViewMatrix);
+    //modelViewMatrix = mult(rotateY, modelViewMatrix);
+    //modelViewMatrix = mult(rotateZ, modelViewMatrix);
+
+    //// position adjustment.
+    //var tMatrix = translate(xPos, yPos, zPos);
+    //modelViewMatrix = mult(tMatrix, modelViewMatrix);
+    //var scaleMatrix = mat4(scaleFactor, 0, 0, 0,
+    //                       0, scaleFactor, 0, 0,
+    //                       0, 0, scaleFactor, 0,
+    //                       0, 0, 0, 1);
+    //modelViewMatrix = mult(scaleMatrix, modelViewMatrix);
+
+    //projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+    //        
+    //gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    //gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+    //    
+
+    //for( var i=0; i<index; i+=3) {
+    //  gl.uniform4fv(fColor, flatten(getRandomColor()));
+    //  gl.drawArrays( gl.TRIANGLES, i, 3 );
+
+    //  gl.uniform4fv(fColor, flatten(black));
+    //  gl.drawArrays( gl.LINE_LOOP, i, 3 );
+    //}
+
+    //window.requestAnimFrame(render);
+}
+
+function processProgram(program) {
+    gl.useProgram( program );
     eye = vec3(0, 0, radius);
 
-    var rotateX = rotate(xAngle, 1, 0, 0) // rotate around the x axis.
-    var rotateY = rotate(yAngle, 0, 1, 0) // rotate around the x axis.
-    var rotateZ = rotate(zAngle, 0, 0, 1) // rotate around the x axis.
+    //var rotateX = rotate(xAngle, 1, 0, 0) // rotate around the x axis.
+    //var rotateY = rotate(yAngle, 0, 1, 0) // rotate around the x axis.
+    //var rotateZ = rotate(zAngle, 0, 0, 1) // rotate around the x axis.
+    var rotateX = rotate(program.xAngle, 1, 0, 0) // rotate around the x axis.
+    var rotateY = rotate(program.yAngle, 0, 1, 0) // rotate around the x axis.
+    var rotateZ = rotate(program.zAngle, 0, 0, 1) // rotate around the x axis.
 
     // angle adjustment.
-    modelViewMatrix = lookAt(eye, at , up);
-    modelViewMatrix = mult(rotateX, modelViewMatrix);
-    modelViewMatrix = mult(rotateY, modelViewMatrix);
-    modelViewMatrix = mult(rotateZ, modelViewMatrix);
+    program.modelViewMatrix = lookAt(eye, at , up);
+    program.modelViewMatrix = mult(rotateX, program.modelViewMatrix);
+    program.modelViewMatrix = mult(rotateY, program.modelViewMatrix);
+    program.modelViewMatrix = mult(rotateZ, program.modelViewMatrix);
 
     // position adjustment.
-    var tMatrix = translate(xPos, yPos, zPos);
-    modelViewMatrix = mult(tMatrix, modelViewMatrix);
-    var scaleMatrix = mat4(scaleFactor, 0, 0, 0,
-                           0, scaleFactor, 0, 0,
-                           0, 0, scaleFactor, 0,
+    var tMatrix = translate(program.xPos, program.yPos, program.zPos);
+    program.modelViewMatrix = mult(tMatrix, program.modelViewMatrix);
+    var scaleMatrix = mat4(program.scaleFactor, 0, 0, 0,
+                           0, program.scaleFactor, 0, 0,
+                           0, 0, program.scaleFactor, 0,
                            0, 0, 0, 1);
-    modelViewMatrix = mult(scaleMatrix, modelViewMatrix);
+    program.modelViewMatrix = mult(scaleMatrix, program.modelViewMatrix);
 
-    projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+    program.projectionMatrix = ortho(left, right, bottom, ytop, near, far);
             
-    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+    gl.uniformMatrix4fv( program.modelViewMatrixLoc, false, flatten(program.modelViewMatrix) );
+    gl.uniformMatrix4fv( program.projectionMatrixLoc, false, flatten(program.projectionMatrix) );
         
 
     for( var i=0; i<index; i+=3) {
-      gl.uniform4fv(fColor, flatten(getRandomColor()));
+      gl.uniform4fv(program.fColor, flatten(getRandomColor()));
       gl.drawArrays( gl.TRIANGLES, i, 3 );
 
-      gl.uniform4fv(fColor, flatten(black));
+      gl.uniform4fv(program.fColor, flatten(black));
       gl.drawArrays( gl.LINE_LOOP, i, 3 );
     }
-
-    //window.requestAnimFrame(render);
-
-
 }
